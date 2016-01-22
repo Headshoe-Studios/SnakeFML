8f2a95cc3953fcbd1c1629e77850fe40bf46ad56
lapinozz <monstrefou@gmail.com>
2016-01-22 16:02:09 -0500

    triangle stripe fro snake

--- SnakeFML/SnakeFML/Snake.cpp
+++ SnakeFML/SnakeFML/Snake.cpp
@@ -1,14 +1,17 @@
 #include "Snake.hpp"
 
-
 Snake::Snake() : sf::Drawable()
 {
-	//hackity hack
-	snakeBody.setSize({ 20, 20 });
-	snakeBody.setPosition(400, 400);
+    snakeBody.setPrimitiveType(sf::TrianglesStrip);
+    snakeBody.append({{0, 0}});
+    snakeBody.append({{0, 20}});
+    snakeBody.append({{20, 0}});
+    snakeBody.append({{20, 20}});
+	setPosition(400, 400);
 }
 
 void Snake::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	target.draw(snakeBody);
+    states.transform *= getTransform();
+	target.draw(snakeBody, states);
 }
--- SnakeFML/SnakeFML/Snake.hpp
+++ SnakeFML/SnakeFML/Snake.hpp
@@ -2,15 +2,15 @@
 
 #include <SFML/Graphics.hpp>
 
-class Snake : public sf::Drawable
+class Snake : public sf::Drawable, public sf::Transformable
 {
 public:
 	Snake();
 	~Snake() = default;
 
 private:
-	
+
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-	sf::RectangleShape snakeBody; //will be trianglestrip or something fancier eventually
+	sf::VertexArray snakeBody;
 };
 
