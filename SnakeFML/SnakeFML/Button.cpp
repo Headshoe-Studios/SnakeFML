d7b1369e7aaed58bee3d7a01cecc4fe34e5306b0
Nyrox <mark.oliver@hotmail.de>
2016-01-23 06:14:55 +0100

    Implemented a Menu

---
+++ SnakeFML/SnakeFML/Button.cpp
@@ -0,0 +1,19 @@
+#include "Button.h"
+
+
+Button::Button(std::function<void()> t_clickCallback) : clickCallback(t_clickCallback) {
+
+}
+
+void Button::processEvent(const sf::Event& event) {
+	switch (event.type) {
+	case sf::Event::MouseButtonPressed:
+		/*
+			Simple check to see whether the button was clicked. 
+			MapPixelToCoords is not required since the GUI should be view independent (Potential STC)
+			*/
+		if (shape.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+			clickCallback();
+		}
+	}
+}
--- SnakeFML/SnakeFML/Main.cpp
+++ SnakeFML/SnakeFML/Main.cpp
@@ -3,16 +3,44 @@
 #include "MouseSpawner.hpp"
 #include <SFML/Window.hpp>
 
+#include "Button.h"
+
 int main()
 {
+	enum E_GAME_STATE {
+		MENU = 0,
+		INGAME = 1,
+
+		CLOSING = 42, // I find this better than forcing a shutdown with window.close() as it prevent's the current execution of the main loop to do strange things, because of the window being closed
+	} gameState = MENU; // Start the game out in the menu
+
+
 	//hardcode size for now...
 	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+
+	float dt = 0; // Delta-Time
+	sf::Clock dtClock;
 
 	//limit to 60 for now. Coil whine is annoying
 	window.setFramerateLimit(60);
 
-	//measure dt
-	sf::Clock dtClock;
+	
+	// Our menu
+	Button playButton([&]() {
+		gameState = INGAME;
+	});
+	playButton.shape.setSize({ 128 * 3 , 128 });
+	playButton.shape.setPosition(static_cast<sf::Vector2f>(window.getSize()) / 2.f - playButton.shape.getSize() / 2.f); // Center the button
+	
+	Button exitButton([&]() {
+		gameState = CLOSING;		
+	});
+	exitButton.shape.setSize({ 128 * 3, 128 });
+	exitButton.shape.setPosition(playButton.shape.getPosition() + sf::Vector2f( 0, 192 )); // Magic numbers yay
+
+	// I guess add a background here
+	// sf::RectangleShape background;
+
 
 	//our snake
 	Snake snake(&window);
@@ -20,27 +48,44 @@ int main()
 	//mouse spawner
 	MouseSpawner spawner(&window);
 
-	while (window.isOpen())
+	while (gameState != CLOSING)
 	{
+		dt = dtClock.restart().asSeconds();
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 			{
-				window.close();
+				gameState = CLOSING;
+			}
+
+			switch (gameState) {
+			case E_GAME_STATE::MENU:
+				playButton.processEvent(event);
+				exitButton.processEvent(event);
 			}
 		}
 
-		//update snake
-		snake.update(dtClock.restart().asSeconds());
+		window.clear(sf::Color(0, 128, 0));
 
-		spawner.checkCollisions(snake);
+		switch (gameState) {
+		case E_GAME_STATE::INGAME:
+			//update snake
+			snake.update(dt);
 
-        window.clear(sf::Color(0,128,0)); // green because... grass?
-		window.draw(snake);
-	    spawner.draw();
+			spawner.checkCollisions(snake);
+			
+			window.draw(snake);
+			spawner.draw();
+			break;
+		case E_GAME_STATE::MENU:
+			window.draw(playButton.shape);
+			window.draw(exitButton.shape);
+			break;
+		}
 		window.display();
 	}
+	window.close();
 
 	return 0;
 };
