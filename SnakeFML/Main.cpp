b52920c35187ab1f2f6649b3807d184ac83db1ef
Kojirion <kojirion@gmail.com>
2016-05-26 01:08:31 +0100

    Draw score only if in-game and restore menu on pressing Esc

--- SnakeFML/Main.cpp
+++ SnakeFML/Main.cpp
@@ -13,31 +13,31 @@
 
 int main()
 {
-	//state
-	enum gameState
-	{
-		MENU,
-		INGAME,
-		EXITING
-	} currentState(MENU); //launch into menu
+    //state
+    enum gameState
+    {
+        MENU,
+        INGAME,
+        EXITING
+    } currentState(MENU); //launch into menu
 
-	//window and framerate control
-	sf::RenderWindow window(sf::VideoMode(800, 800), "SnakeFML");
-	window.setFramerateLimit(60);
-	sf::Image icon;
+    //window and framerate control
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SnakeFML");
+    window.setFramerateLimit(60);
+    sf::Image icon;
     icon.loadFromFile("icon.png");
-	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-	sf::Clock	dtClock;
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    sf::Clock	dtClock;
 
-	//Objects
+    //Objects
     Options         options;
-	Score			score("KBZipaDeeDooDah.ttf");
-	World			world("Grass.png", { 1000,1000 });
+    Score			score("KBZipaDeeDooDah.ttf");
+    World			world("Grass.png", { 1000,1000 });
     Snake			snake(window,world,score,"SnakeHead.png", options);
-	MouseSpawner	spawner(&window,"Mouse.png", snake);
-//	Button			playButton([&]() {currentState = INGAME; }, "PlayButton.png");
-//	Button			quitButton([&]() {currentState = EXITING; }, "QuitButton.png");
-//	Menu			menu(window,{ playButton, quitButton });
+    MouseSpawner	spawner(&window,"Mouse.png", snake);
+    //	Button			playButton([&]() {currentState = INGAME; }, "PlayButton.png");
+    //	Button			quitButton([&]() {currentState = EXITING; }, "QuitButton.png");
+    //	Menu			menu(window,{ playButton, quitButton });
 
     sfg::SFGUI sfgui;
     sfg::Desktop desktop;
@@ -84,82 +84,84 @@ int main()
     });
     makeMenuButton("Quit", [&currentState, &menuBox]{
         currentState = EXITING;
-        menuBox->Show(true);
     });
 
-	while (window.isOpen())
-	{
-		if (currentState == EXITING)
-		{
-			window.close();
-			continue;
-		}
-		auto dt(dtClock.restart().asSeconds());
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				currentState = EXITING;
-			}
+    while (window.isOpen())
+    {
+        if (currentState == EXITING)
+        {
+            window.close();
+            continue;
+        }
+        auto dt(dtClock.restart().asSeconds());
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                currentState = EXITING;
+            }
 
-			switch (currentState)
-			{
-			case MENU:
+            switch (currentState)
+            {
+            case MENU:
                 desktop.HandleEvent(event);
                 //menu.handleEvent(event);
-				break;
-			case INGAME:
-				if (event.type == sf::Event::KeyPressed)
-				{
-					switch (event.key.code)
-					{
-						//escape to pause
-					case sf::Keyboard::Escape:
-						currentState = MENU;
-						break;
-					default:
-						break;
-					}
-				}
-				break;
-			default:
-				break;
-			}
-		}
+                break;
+            case INGAME:
+                if (event.type == sf::Event::KeyPressed)
+                {
+                    switch (event.key.code)
+                    {
+                    //escape to pause
+                    case sf::Keyboard::Escape:
+                        currentState = MENU;
+                        menuBox->Show(true);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+        }
 
         desktop.Update(dt);
 
-		window.clear(sf::Color(50,150, 255));	//because Sea!
+        window.clear(sf::Color(50,150, 255));	//because Sea!
 
-		if (currentState == INGAME)
-		{
-			//update objects if in game (i.e. not paused)
-			snake.update(dt);
-			spawner.checkCollisions();
-			spawner.spawn();
-		}
+        if (currentState == INGAME)
+        {
+            //update objects if in game (i.e. not paused)
+            snake.update(dt);
+            spawner.checkCollisions();
+            spawner.spawn();
+        }
 
-		//draw the essentials (with the correct view)
+        //draw the essentials (with the correct view)
         if (options.getMovingCamera())
             window.setView(snake.getView());
-		window.draw(world);
-		spawner.draw();
-		window.draw(snake);
+        window.draw(world);
+        spawner.draw();
+        window.draw(snake);
 
-		//default view for score (and menu, if needed)
-		window.setView(window.getDefaultView());
-		window.draw(score);
+        //default view for score (and menu, if needed)
+        if (currentState != MENU){
+            window.setView(window.getDefaultView());
+            window.draw(score);
+        }
 
-//		if (currentState == MENU)
-//			window.draw(menu);
+        //		if (currentState == MENU)
+        //			window.draw(menu);
 
         sfgui.Display(window);
 
-		window.display();
-		sf::sleep(sf::seconds(0));
-	}
-	window.close();
+        window.display();
+        sf::sleep(sf::seconds(0));
+    }
+    window.close();
 
-	return 0;
+    return 0;
 };
